0b80f0b15e34e4677a1fc2f5f40e582b88e742a8
Ali Ghorbani <aghorbani@bluebeam.com>
2017-06-16 10:52:35 -0700

    Added C# play app

---
+++ Features/DemoFontSimilarity/DemoFontSimilarity/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace DemoFontSimilarity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.refFonts = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelRef = new System.Windows.Forms.Label();
+            this.labelFont1 = new System.Windows.Forms.Label();
+            this.labelFont2 = new System.Windows.Forms.Label();
+            this.labelFont3 = new System.Windows.Forms.Label();
+            this.labelFont4 = new System.Windows.Forms.Label();
+            this.labelFont5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFontNames = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // refFonts
+            // 
+            this.refFonts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.refFonts.FormattingEnabled = true;
+            this.refFonts.Location = new System.Drawing.Point(0, 0);
+            this.refFonts.Name = "refFonts";
+            this.refFonts.Size = new System.Drawing.Size(903, 24);
+            this.refFonts.TabIndex = 0;
+            this.refFonts.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.labelRef, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelFont1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelFont2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelFont3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelFont4, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.labelFont5, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblFontNames, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 5);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.70854F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.52763F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.63819F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.70854F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.70854F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.70854F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(903, 665);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // labelRef
+            // 
+            this.labelRef.AutoSize = true;
+            this.labelRef.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelRef.Location = new System.Drawing.Point(3, 0);
+            this.labelRef.Name = "labelRef";
+            this.labelRef.Size = new System.Drawing.Size(445, 107);
+            this.labelRef.TabIndex = 0;
+            // 
+            // labelFont1
+            // 
+            this.labelFont1.AutoSize = true;
+            this.labelFont1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelFont1.Location = new System.Drawing.Point(3, 107);
+            this.labelFont1.Name = "labelFont1";
+            this.labelFont1.Size = new System.Drawing.Size(445, 100);
+            this.labelFont1.TabIndex = 1;
+            // 
+            // labelFont2
+            // 
+            this.labelFont2.AutoSize = true;
+            this.labelFont2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelFont2.Location = new System.Drawing.Point(3, 207);
+            this.labelFont2.Name = "labelFont2";
+            this.labelFont2.Size = new System.Drawing.Size(445, 113);
+            this.labelFont2.TabIndex = 2;
+            // 
+            // labelFont3
+            // 
+            this.labelFont3.AutoSize = true;
+            this.labelFont3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelFont3.Location = new System.Drawing.Point(3, 320);
+            this.labelFont3.Name = "labelFont3";
+            this.labelFont3.Size = new System.Drawing.Size(445, 107);
+            this.labelFont3.TabIndex = 3;
+            // 
+            // labelFont4
+            // 
+            this.labelFont4.AutoSize = true;
+            this.labelFont4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelFont4.Location = new System.Drawing.Point(3, 427);
+            this.labelFont4.Name = "labelFont4";
+            this.labelFont4.Size = new System.Drawing.Size(445, 107);
+            this.labelFont4.TabIndex = 4;
+            // 
+            // labelFont5
+            // 
+            this.labelFont5.AutoSize = true;
+            this.labelFont5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelFont5.Location = new System.Drawing.Point(3, 534);
+            this.labelFont5.Name = "labelFont5";
+            this.labelFont5.Size = new System.Drawing.Size(445, 107);
+            this.labelFont5.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(454, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(446, 100);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(454, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(446, 113);
+            this.label3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(454, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(446, 107);
+            this.label4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(454, 427);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(446, 107);
+            this.label5.TabIndex = 10;
+            // 
+            // lblFontNames
+            // 
+            this.lblFontNames.AutoSize = true;
+            this.lblFontNames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblFontNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFontNames.Location = new System.Drawing.Point(3, 641);
+            this.lblFontNames.Name = "lblFontNames";
+            this.lblFontNames.Size = new System.Drawing.Size(445, 24);
+            this.lblFontNames.TabIndex = 13;
+            this.lblFontNames.Text = "label7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(454, 534);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 107);
+            this.label1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 689);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.refFonts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox refFonts;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelRef;
+        private System.Windows.Forms.Label labelFont1;
+        private System.Windows.Forms.Label labelFont2;
+        private System.Windows.Forms.Label labelFont3;
+        private System.Windows.Forms.Label labelFont4;
+        private System.Windows.Forms.Label labelFont5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFontNames;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Features/DemoFontSimilarity/DemoFontSimilarity/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoFontSimilarity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadSimilarityMatrix();
+            ReadFontNames();
+            refFonts.DataSource = new BindingSource(similarity_dict, null);
+            refFonts.DisplayMember = "Key";
+            refFonts.ValueMember = "Value";
+        }
+
+        private void ReadFontNames()
+        {
+            var fontNamesLines = File.ReadAllLines(@"..\..\fontNames.txt");
+            FontNames = new List<string>(fontNamesLines);
+        }
+
+        IList<string> FontNames = null;
+        Dictionary<string, IList<string>> similarity_dict = null;
+
+        private void ReadSimilarityMatrix()
+        {
+            similarity_dict = new Dictionary<string, IList<string>>();
+
+            using (var fs = File.OpenRead(@"..\..\similarity_df.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                var line_header = reader.ReadLine();
+                var ref_font_names = line_header.Split(',');
+
+                foreach (var ref_font_name in ref_font_names.Skip(1))
+                    similarity_dict[ref_font_name] = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var names = line.Split(',');
+
+                    int nameCounter = 1;
+                    foreach (var ref_font_name in ref_font_names.Skip(1))
+                    {
+                        similarity_dict[ref_font_name].Add(names[nameCounter++]);
+                    }
+                }
+            }
+        }
+
+        private Font GetFont(string fileName)
+        {
+            PrivateFontCollection FontCollection = new PrivateFontCollection();
+            string fontPath = string.Format("{0}\\{1}.ttf", FontsFolder, fileName);
+            if (!File.Exists(fontPath))
+                fontPath = string.Format("{0}\\{1}.otf", FontsFolder, fileName);
+
+            if (!File.Exists(fontPath))
+                return null;
+
+            FontCollection.AddFontFile(fontPath);
+            return new Font((FontFamily)FontCollection.Families[0], 24f);
+        }
+
+        private const string FontsFolder = @"C:\Users\aghorbani\Dropbox\MyFonts\";
+
+        private void DrawLabelText(Label lbl, string font_name, Color color)
+        {
+            var font = GetFont(font_name);
+            if (font == null)
+                return;
+
+            Graphics g = lbl.CreateGraphics();
+            g.Clear(SystemColors.Control);
+            SolidBrush myBrush = new SolidBrush(color);
+            g.DrawString("quick brown fox jumps over the lazy dog!", font, myBrush, new
+               RectangleF(10, 10, 1000, 500));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string referenceFontName = null;
+            IList<string> replacementFonts = null;
+
+            if (this.refFonts.SelectedValue is IList<string>)
+            {
+                referenceFontName = this.refFonts.Text;
+                replacementFonts = (IList<string>)this.refFonts.SelectedValue;
+            }
+            else
+            {
+                var similarFonts = (KeyValuePair<string, IList<string>>)this.refFonts.SelectedValue;
+                referenceFontName = similarFonts.Key;
+                replacementFonts = similarFonts.Value;
+            }
+
+            lblFontNames.Text = string.Join(",", replacementFonts.ToArray());
+
+            DrawLabelText(labelRef, referenceFontName, Color.Blue);
+            DrawLabelText(labelFont1, replacementFonts[0], Color.Green);
+            DrawLabelText(labelFont2, replacementFonts[1], Color.Green);
+            DrawLabelText(labelFont3, replacementFonts[2], Color.Green);
+            DrawLabelText(labelFont4, replacementFonts[3], Color.Green);
+            DrawLabelText(labelFont5, replacementFonts[4], Color.Green);
+
+            select_random_fonts();
+        }
+
+        private void select_random_fonts()
+        {
+            Random r = new Random();
+
+            DrawLabelText(label1, FontNames[r.Next(FontNames.Count)], Color.Red);
+            DrawLabelText(label2, FontNames[r.Next(FontNames.Count)], Color.Red);
+            DrawLabelText(label3, FontNames[r.Next(FontNames.Count)], Color.Red);
+            DrawLabelText(label4, FontNames[r.Next(FontNames.Count)], Color.Red);
+            DrawLabelText(label5, FontNames[r.Next(FontNames.Count)], Color.Red);
+        }
+    }
+}
